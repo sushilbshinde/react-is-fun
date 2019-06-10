36eb4cee63094d3968171d9f83a8cabdabb981b0
Sushi Shinde <sushilbshinde@gmail.com>
2019-06-10 12:38:06 +0530

    add component properties

--- src/index.js
+++ src/index.js
@@ -5,10 +5,16 @@ class Message extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello Everyone</h1>
+                <h1 style={{color: this.props.color}}>
+                    {this.props.msg}
+                </h1>
+                <p>I'll check back in {this.props.minutes} minutes</p>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Message />, document.getElementById('root'))
+ReactDOM.render(
+    <Message color="blue" msg="how are you?" minutes={5}/>, 
+    document.getElementById('root')
+)
