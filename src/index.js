5e300af210226d6efa7c4d984fadb6b7ac24c3fb
Sushi Shinde <sushilbshinde@gmail.com>
2019-06-14 10:07:59 +0530

    Using setState

--- src/index.js
+++ src/index.js
@@ -18,17 +18,19 @@ const Book = ({title, author, pages}) => {
 }
 
 class Library extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            open: true
-        }
+    state = { open: true }
+    
+    toggleOpenClosed = () => {
+        this.setState(prevState => ({
+            open: !prevState.open
+        }))
     }
     render() {
         const { books } = this.props;
         return (
             <div>
                 <h1>The library is {this.state.open ? 'open' : 'closed' }</h1>
+                <button onClick={this.toggleOpenClosed}>Change</button>
                 {
                     books.map(
                         (book, i) =>
